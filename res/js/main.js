aa8372e888178b8341530a37a2df16c3b01cec07
Karasmore <okarasek13@seznam.cz>
2023-01-14 20:10:34 +0100

    pipes trying

--- res/js/main.js
+++ res/js/main.js
@@ -1,9 +1,14 @@
 let start = document.getElementById("start");
 let wrap = document.getElementById("wrapper");
+
 let gameStart = false;
 let coolDown = true;
-const jump = 5.5;
 let over = false;
+let gameSpeed = 3;
+let secondPosition = 1920;
+
+const jump = 5.5;
+
 
 
 const canvas = document.querySelector("canvas");
@@ -11,13 +16,16 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 1920;
 canvas.height = 969;
-let height= (canvas.height / 2) - 50;
+let playerPosition= (canvas.height / 2) - 50;
 
 start.onclick = () => {
+    run = true;
     wrapper.style.display = "none";
+    
     canvas.style.display = "flex";
-
+  
 }
+
 
 class myPlayer {
     constructor() {
@@ -30,16 +38,57 @@ class myPlayer {
     draw(y) {
 
         ctx.drawImage(birdImage, this.position.x, y, this.width, this.height)
-        ctx.fillStyle = "#000";
       
     };
 }
+
+ 
+
+
+class myPipe {
+    constructor() {
+        this.position = { x: canvas.width, y:0};
+        this.width = 100;
+        this.bot = (Math.random()* canvas.height/2)+150;
+        this.top = canvas.height - this.bot -250;
+        this.bgPos = 0;
+       
+        
+    }
+ 
+
+   draw(){
+    
+    ctx.fillStyle = "green";
+    ctx.fillRect(this.position.x,this.position.y, this.width ,  this.top);
+    ctx.fill();
+    ctx.fillRect(this.position.x,canvas.height - this.bot, this.width , this.bot);
+    ctx.fill();
+   
+   }
+   generate(){
+    pipe.position.x = pipe.bgPos + canvas.width;
+   }
+   
+    
+     
+  
+      //this.position.x = (this.position.x - gameSpeed);
+   
+   
+   
+}
+ 
+
+
+
+
 
 let birdImage = new Image;
 
 birdImage.src = "./res/img/bird2.png";
 const player = new myPlayer(birdImage);
-
+const pipe = new myPipe();
 
 
 const begin= () =>{
@@ -51,7 +100,8 @@ const begin= () =>{
 
 document.addEventListener('keydown', (x) => {
     
-    if (x.code === "Space" && coolDown && height > 85&& !over) {
+    if (x.code === "Space" && coolDown && playerPosition > 85&& !over&&
+     run == true) {
         if (!gameStart) {
            
             gameStart = !gameStart;
@@ -59,7 +109,7 @@ document.addEventListener('keydown', (x) => {
 
         const smoothJump = setInterval(() => {
           
-            height -= jump;
+            playerPosition -= jump;
 
         }, 0.75);
 
@@ -67,7 +117,7 @@ document.addEventListener('keydown', (x) => {
         setTimeout(() => {
             coolDown = true;
             clearInterval(smoothJump);
-        }, 175)
+        }, 175) 
     }
 });
 
@@ -76,6 +126,7 @@ const backgroundCanvas = () => {
     bgImage.src = "./res/img/bg.png";
     ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height)
 };
+
 
 
 const score = () =>{
@@ -88,29 +139,58 @@ const score = () =>{
     ctx.fill();
 
     ctx.fillStyle = "#fff";
-    ctx.font = "bold 25px sans-serif"
+    ctx.font = "bold 27px sans-serif"
     ctx.fillText("Score ", canvas.width/2 -200,350);
     ctx.fillText("Best", canvas.width/2 + 130,350);
+   
+    ctx.filter = 'grayscale(1)';
+   
+
 }
 
 function gameLoop() {
-
+ 
     backgroundCanvas();
+  
+    
     if (!gameStart) {
         begin();  
+     
     }
   
-    player.draw(height);
-    if (gameStart && coolDown) {
-        height += jump / 2;
-    }
-    if(height >= canvas.height- 80){
-        score();
-        over = true;
-        player = none;
+    player.draw(playerPosition);
     
+    if (gameStart && coolDown ) {
+        playerPosition += jump / 2;
+        
+    }
+    if(gameStart ){
+           pipe.draw();
+           pipe.generate();
+           pipe.bgPos -= gameSpeed;
+            }
+   
+   
+    if(playerPosition >= canvas.height- 80){
       
+       score();
+       // playerPosition = 889 ;
+       over = true; 
+ 
+       
     }   
+    if(pipe.position.x <1020 || secondPosition<1020){
+        pipe.position.x = secondPosition;
+        this.bot = (Math.random()* canvas.height/2)+150;
+       
+        pipe.draw();
+        pipe.generate();
+      secondPosition-=3;
+        console.log(".x");
+         
+    }
+   console.log(pipe.position.x);
+   
     requestAnimationFrame(gameLoop);
 }
 
